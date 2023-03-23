cef8c130a5309d69276102e3e644bf17b96b92f4
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-03-23 16:42:23 -0400

    feat: install redux syntax for front end rendering

--- client/index.js
+++ client/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM, { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './store/index';
 
 // make sure this react dom create refers to the same id of the div in the index.html file
 ReactDOM.createRoot(document.getElementById('app')).render(
-  <div>Hello, world</div>
+  <Provider store={store}>
+    <div>Hello, world</div>
+  </Provider>
 );
