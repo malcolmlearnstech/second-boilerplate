0106af7f699823522fd3d631319314b2df965fe9
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-03-24 23:09:15 -0400

    feat: add style into rendering file

--- client/index.js
+++ client/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM, { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './store/index';
+import './index.css';
 
 // make sure this react dom create refers to the same id of the div in the index.html file
 ReactDOM.createRoot(document.getElementById('app')).render(
   <Provider store={store}>
-    <div>Hello, world</div>
+    <div className="indexStyle">
+      <p>Hello, World</p>
+    </div>
   </Provider>
 );
